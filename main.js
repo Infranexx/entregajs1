109765ebf00d5448b603419cbf39c6624999e8fc
Lucas Biscotti <lucasbiscotti1602@gmail.com>
2024-06-18 22:16:20 -0400

    Primer commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let producto = '' ;
+let cantidad = 0 ;
+let precio = 0 ;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const productos = [
+    { nombre: "Detergente", precio: 5500 },
+    { nombre: "Lavalozas", precio: 4000 },
+    { nombre: "Limpia Pisos aromas", precio: 3500 },
+    { nombre: "Suavizante", precio: 6000 },
+    { nombre: "Ambiental aromas", precio: 5500 },
+    {nombre: "Desmanchador líquido", precio: 6000 },
+] ;
+
+let carrito = []
+
+let seleccion = prompt("¡Bienvenido! ¿Desea adquirir nuestros productos de aseo/limpieza?")
+
+while(seleccion != "si" && seleccion != "no" )
+    {alert ('Por favor, escriba "si" o "no"')
+      
+
+    }
